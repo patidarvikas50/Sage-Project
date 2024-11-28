c1048f9394aaca44fe2d2700c3989ed8fcb812bf
Vikas Patidar <patidarvikas50@gmail.com>
2024-11-28 21:23:58 +0530

    Initial commit with frontend and backend

---
+++ task-management/frontend/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-white shadow h-14'>
+      <span className="logo1" >Task</span>
+      <span className="logo2">Management</span>
+      </div>
+  )
+}
+
+export default Navbar
